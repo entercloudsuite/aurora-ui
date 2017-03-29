4a4c3ba64787efa62c6b3015a1216555d190b4cb
Federico Minzoni <fminzoni@enter.it>
2017-03-29 15:11:29 +0200

    add test env

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,7 +75,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         hostname: '0.0.0.0',
-        livereload: 35729
+        //livereload: 35729
       },
       livereload: {
         options: {
