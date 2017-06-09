cee34d91a3020e4057d63730d0fedd7c4bcb7ea6
drymonsoon <tirabassigiacom@gmail.com>
2017-06-09 10:26:54 +0200

    fix ip auth

--- app/scripts/config/config.ts
+++ app/scripts/config/config.ts
@@ -1,8 +1,8 @@
 /// <reference path="../_all.ts" />
 
 app.constant('config', {
-	'AUTH_URL': "http://185.48.34.118:3000/core/identity",
-	'OS_URL': "http://185.48.34.118:3000/core",
+	'AUTH_URL': "http://185.48.33.24:3000/core/identity",
+	'OS_URL': "http://185.48.33.24:3000/core",
 	'BRIDGE_URL': "http://localhost/bridge/bridge.php"
 	}
 )
